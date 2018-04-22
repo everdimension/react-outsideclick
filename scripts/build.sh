6c48dcfe5072cb18f40b23a1850f0752150fa62e
everdimension <everdimension@gmail.com>
2018-04-22 11:20:31 -0400

    Build library with rollup
    remove `./scripts` folder

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf dist
-./node_modules/.bin/webpack
-./node_modules/.bin/webpack --output-filename=dist/ReactOutsideClick.min.js --optimize-minimize
-
-rm -rf lib
-./node_modules/.bin/babel src --out-dir lib
